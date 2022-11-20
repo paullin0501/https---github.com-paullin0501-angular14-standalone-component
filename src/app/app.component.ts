4035e16a5a44d4ae8a021363c47d1e73dfba5f40
paul <paullin050182@gmail.com>
2022-11-20 21:37:04 +0800

    change all component to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { WelcomeComponent } from './welcome/welcome.component';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
+  imports: [WelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+//import { AnalyticsService } from './app/shared/analytics.service';
+import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{
+  providers: [
+    //AnalyticsService
+  ]
+}
+  
+  );
